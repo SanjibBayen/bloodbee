18f4ba34491343a2cef00499dda3bc3886766b6c
Ayush-Alam45 <ayushalam45@gmail.com>
2025-03-18 01:50:09 +0530

    refactor LoginDonor component for improved accessibility and styling; update form structure and labels

--- src/pages/donor/LoginDonor.jsx
+++ src/pages/donor/LoginDonor.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { lable } from '../../assets/img';
-import { Link } from 'react-router-dom'; // Corrected import
+import { Link } from 'react-router-dom';
 
 const LoginDonor = () => {
     const handleSubmit = (event) => {
-        event.preventDefault(); // Prevent the default form submission behavior
-        // Optionally, you can add any other logic here, such as showing a message to the user
+        event.preventDefault();
+        // Additional login logic can be added here
     };
-    return (
-        <div>
-            <div className="text-center p-3 container-md">
 
-            </div>
-            <div id="intro-example" className="text-center bg-image align-items-center">
+    return (
+        <div className="container-md text-center p-3">
+            <div className="text-center bg-image align-items-center">
                 <section>
                     <div className="container h-100">
                         <div className="row d-flex justify-content-center align-items-center h-100">
@@ -21,37 +18,35 @@ const LoginDonor = () => {
                                     <div className="card-body p-md-5">
                                         <div className="row justify-content-center">
                                             <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                                                <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Welcome Back Our beloved Blood Donor!</p>
-                                                <form className="mx-1 mx-md-4" onSubmit={handleSubmit}> 
-                                                    <div className="d-flex flex-row align-items-center mb-4">
-                                                        <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                                                        <div className="form-outline flex-fill mb-0">
-                                                            <input type="email" name="email" id="form3Example3c" className="form-control" placeholder="Your email" />
-                                                        </div>
+                                                <h1 className="text-center fw-bold mb-5">
+                                                    Welcome Back, Beloved <span style={{ color: 'red' }}>Blood</span> Donor!
+                                                </h1>
+                                                <form className="mx-1 mx-md-4" onSubmit={handleSubmit}>
+                                                    <div className="mb-4">
+                                                        <label htmlFor="email" className="form-label">Email</label>
+                                                        <input type="email" name="email" id="email" className="form-control" placeholder="Your email" required />
                                                     </div>
-                                                    <div className="d-flex flex-row align-items-center mb-4">
-                                                        <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                                                        <div className="form-outline flex-fill mb-0">
-                                                            <input type="password" name="password" id="form3Example4c" className="form-control" placeholder="Password" />
-                                                        </div>
+                                                    <div className="mb-4">
+                                                        <label htmlFor="password" className="form-label">Password</label>
+                                                        <input type="password" name="password" id="password" className="form-control" placeholder="Password" required />
                                                     </div>
-                                                    <div className="form-check d-flex justify-content-center mb-5">
-                                                        <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
-                                                        <label className="form-check-label" htmlFor="form2Example3">
-                                                            I agree all statements in <a href="#!">Terms of service</a>
+                                                    <div className="form-check d-flex justify-content-center mb-4">
+                                                        <input className="form-check-input me-2" type="checkbox" id="terms" required />
+                                                        <label className="form-check-label" htmlFor="terms">
+                                                            I agree to all statements in <a href="#!" style={{ color: 'red' }}>Terms of Service</a>
                                                         </label>
                                                     </div>
-                                                    <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                                        <button name="login" className="btn btn-primary btn-lg">Login</button>
+                                                    <div className="d-flex justify-content-center">
+                                                        <button type="submit" className="btn btn-primary btn-lg">Login</button>
                                                     </div>
-                                                    <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                                        <p className="small mb-0">Don't have account? <Link to="/register-donor">Create an account</Link></p>
+                                                    <div className="text-center mt-3">
+                                                        <p>Don't have an account? <Link to="/register-donor" style={{ color: 'red' }}>Create an account</Link></p>
                                                     </div>
                                                 </form>
                                             </div>
                                             <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                                                <Link to={'/BloodDonationUI'}>
-                                                    <img src={lable} className="img-fluid rounded-4" alt="Sample image" />
+                                                <Link to='/BloodDonationUI'>
+                                                    <img src="https://static.vecteezy.com/system/resources/thumbnails/008/190/897/small_2x/human-blood-donate-on-white-background-free-vector.jpg" className="img-fluid rounded-4" alt="Blood Donation" />
                                                 </Link>
                                             </div>
                                         </div>
@@ -64,6 +59,6 @@ const LoginDonor = () => {
             </div>
         </div>
     );
-}
+};
 
 export default LoginDonor;
